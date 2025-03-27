25bf1ae598c408e132251109da46892c0df80733
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-27 12:36:50 -0500

    feat: add UserService and its implementation

---
+++ src/main/java/com/example/demo/users/application/services/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.users.application.services;
+
+import com.example.demo.users.application.dto.request.CreateUserRequest;
+import com.example.demo.users.application.dto.response.CreateUserResponse;
+
+public interface UserService {
+    CreateUserResponse create(CreateUserRequest request);
+}
---
+++ src/main/java/com/example/demo/users/application/services/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.users.application.services.impl;
+
+import com.example.demo.commons.configurations.utils.Constants;
+import com.example.demo.users.application.dto.request.CreateUserRequest;
+import com.example.demo.users.application.dto.response.CreateUserResponse;
+import com.example.demo.users.application.mappers.UserDtoMapper;
+import com.example.demo.users.application.services.UserService;
+import com.example.demo.users.domain.ports.in.UserServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserServicePort userServicePort;
+    private final UserDtoMapper userDtoMapper;
+
+    @Override
+    public CreateUserResponse create(CreateUserRequest request) {
+        userServicePort.create(userDtoMapper.requestToModel(request));
+        return new CreateUserResponse(Constants.CREATE_USERS_RESPONSE_MESSAGE, LocalDateTime.now());
+    }
+}
