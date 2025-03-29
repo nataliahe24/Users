c733e248c6201b2ecd23b37ee17e82549465250c
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 10:14:45 -0500

    feat: add bean PasswordEncoder and PasswordEncoderPort

--- src/main/java/com/example/demo/commons/configurations/beans/BeanConfiguration.java
+++ src/main/java/com/example/demo/commons/configurations/beans/BeanConfiguration.java
@@ -1,13 +1,17 @@
 package com.example.demo.commons.configurations.beans;
 import com.example.demo.users.domain.ports.in.UserServicePort;
+import com.example.demo.users.domain.ports.out.PasswordEncoderPort;
 import com.example.demo.users.domain.ports.out.UserPersistencePort;
 import com.example.demo.users.domain.usecases.UserUseCase;
+import com.example.demo.users.infrastructure.adapters.persistence.PasswordEncoderAdapter;
 import com.example.demo.users.infrastructure.adapters.persistence.UserPersistenceAdapter;
 import com.example.demo.users.infrastructure.mappers.UserEntityMapper;
 import com.example.demo.users.infrastructure.repositories.mysql.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,12 +20,21 @@ public class BeanConfiguration {
     private final UserEntityMapper userEntityMapper;
 
     @Bean
-    public UserServicePort userServicePort() {
-        return new UserUseCase(userPersistencePort());
+    public UserServicePort userServicePort(UserPersistencePort userPersistencePort,PasswordEncoderPort passwordEncoderPort) {
+        return new UserUseCase(userPersistencePort, passwordEncoderPort);
     }
 
     @Bean
     public UserPersistencePort userPersistencePort() {
         return new UserPersistenceAdapter(userRepository, userEntityMapper);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public PasswordEncoderPort passwordEncoderPort(PasswordEncoder passwordEncoder) {
+        return new PasswordEncoderAdapter(passwordEncoder);
     }
 }
