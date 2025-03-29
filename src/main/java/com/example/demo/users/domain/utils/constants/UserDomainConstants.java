9796b629052c655c3b1948d95f8f4d448d182a95
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 14:59:55 -0500

    feat: add constants FIELD_PHONE_NUMBER_VALID_CHARACTERS, FIELD_PHONE_NUMBER_EXCEEDED_CHARACTERS y FIELD_PHONE_CHARACTERS_INVALID.

--- src/main/java/com/example/demo/users/domain/utils/constants/UserDomainConstants.java
+++ src/main/java/com/example/demo/users/domain/utils/constants/UserDomainConstants.java
@@ -1,8 +1,8 @@
 package com.example.demo.users.domain.utils.constants;
 
 
-public final class DomainConstants {
-    private DomainConstants() {
+public final class UserDomainConstants {
+    private UserDomainConstants() {
         throw new IllegalStateException("Utility class");
     }
     public static final String FIELD_FIRSTNAME_NULL_MESSAGE = "Atributo 'nombre' no puede ser nulo";
@@ -11,8 +11,12 @@ public final class DomainConstants {
     public static final String FIELD_PHONE_NUMBER_NULL_MESSAGE = "Atributo 'Número de teléfono' no puede ser nulo";
     public static final String FIELD_BIRTH_DATE_NULL_MESSAGE = "Atributo 'Fecha de nacimiento' no puede ser nulo";
     public static final String FIELD_EMAIL_NULL_MESSAGE = "Atributo 'Email' no puede ser nulo";
+    public static final String FIELD_PASSWORD_NULL_MESSAGE = "El campo 'contraseña' no puede ser nulo";
     public static final String FIELD_ROLE_NULL_MESSAGE = "Atributo 'Rol' no puede ser nulo";
     public static final Integer MIN_AGE_ALLOWED = 18;
     public static final Integer FIELD_PHONE_NUMBER_SYMBOL = 1;
     public static final Integer FIELD_PHONE_NUMBER_CHARACTERS = 13;
+    public static final String FIELD_PHONE_CHARACTERS_INVALID = "++12345678901";
+    public static final String FIELD_PHONE_NUMBER_EXCEEDED_CHARACTERS = "12345678901234";
+    public static final String FIELD_PHONE_NUMBER_VALID_CHARACTERS = "1234567890123";
 }
