35664a366711c9fd21fd08addcfe9d4c3508248c
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 14:58:48 -0500

    feat: add UserModelTest

--- src/test/java/com/example/demo/users/domain/model/UserModelTest.java
+++ src/test/java/com/example/demo/users/domain/model/UserModelTest.java
@@ -1,9 +1,10 @@
 package com.example.demo.users.domain.model;
 
+import com.example.demo.users.domain.exceptions.PhoneNumberCharacterInvalid;
+import com.example.demo.users.domain.exceptions.PhoneNumberExceededException;
+import com.example.demo.users.domain.utils.constants.UserDomainConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -30,7 +31,7 @@ class UserModelTest {
         birthDate = LocalDate.of(1990, 1, 1);
         email = "johndoe@example.com";
         password = "securepassword";
-        role = "USER";
+        role = "SELLER";
 
     }
 
@@ -46,9 +47,92 @@ class UserModelTest {
         assertEquals(email, user.getEmail());
         assertEquals(role, user.getRole());
         assertNotNull(user.getPassword());
-        assertNotEquals(password, user.getPassword());
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        assertNotNull(user.getPassword());
-        assertTrue(passwordEncoder.matches(password, user.getPassword()), "La contraseña codificada no coincide.");
+        assertEquals(password, user.getPassword());
+
     }
+
+    @Test
+    void shouldPhoneNumberHas13CharactersOrLess() {
+        String validPhoneNumber = UserDomainConstants.FIELD_PHONE_NUMBER_VALID_CHARACTERS;
+
+        assertDoesNotThrow(() -> new UserModel(firstName, lastName, identityDocument, validPhoneNumber, birthDate, email, password, role));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPhoneNumberExceeds13Characters() {
+        String invalidPhoneNumber = UserDomainConstants.FIELD_PHONE_NUMBER_EXCEEDED_CHARACTERS;
+
+        assertThrows(PhoneNumberExceededException.class, () -> new UserModel(firstName, lastName, identityDocument, invalidPhoneNumber, birthDate, email, password, role));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPhoneNumberHasCharactersInvalid() {
+        String invalidPhoneNumber = UserDomainConstants.FIELD_PHONE_CHARACTERS_INVALID;
+        assertThrows(PhoneNumberCharacterInvalid.class, () -> new UserModel(firstName, lastName, identityDocument, invalidPhoneNumber, birthDate, email, password, role));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenFirstNameIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setFirstName(null));
+        assertEquals(UserDomainConstants.FIELD_FIRSTNAME_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenLastNameIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setLastName(null));
+        assertEquals(UserDomainConstants.FIELD_LASTNAME_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenIdentityDocumentIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setIdentityDocument(null));
+        assertEquals(UserDomainConstants.FIELD_IDENTITY_DOCUMENT_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPhoneNumberIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setPhoneNumber(null));
+        assertEquals(UserDomainConstants.FIELD_PHONE_NUMBER_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenBirthDateIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setBirthDate(null));
+        assertEquals(UserDomainConstants.FIELD_BIRTH_DATE_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setEmail(null));
+        assertEquals(UserDomainConstants.FIELD_EMAIL_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPasswordIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setPassword(null));
+        assertEquals(UserDomainConstants.FIELD_PASSWORD_NULL_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRoleIsNull() {
+        UserModel newUser = new UserModel(firstName, lastName, identityDocument, phoneNumber, birthDate, email, password, role);
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> newUser.setRole(null));
+        assertEquals(UserDomainConstants.FIELD_ROLE_NULL_MESSAGE, exception.getMessage());
+    }
+
 }
