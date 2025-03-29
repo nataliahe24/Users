49fd0123726e93fbfb39d97e17b5bcd333f6392e
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 10:09:47 -0500

    fear: add PasswordEncoderPort for use encode() method

--- src/main/java/com/example/demo/users/domain/usecases/UserUseCase.java
+++ src/main/java/com/example/demo/users/domain/usecases/UserUseCase.java
@@ -1,25 +1,31 @@
 package com.example.demo.users.domain.usecases;
 
-import com.example.demo.users.domain.exceptions.SellerUserAlreadyExistsException;
+import com.example.demo.users.domain.exceptions.UserAlreadyExistsException;
 import com.example.demo.users.domain.model.UserModel;
 import com.example.demo.users.domain.ports.in.UserServicePort;
+import com.example.demo.users.domain.ports.out.PasswordEncoderPort;
 import com.example.demo.users.domain.ports.out.UserPersistencePort;
 
 public class UserUseCase implements UserServicePort {
     private final UserPersistencePort userPersistencePort;
+    private final PasswordEncoderPort passwordEncoderPort;
 
-    public UserUseCase(UserPersistencePort userPersistencePort) {
+    public UserUseCase(UserPersistencePort userPersistencePort, PasswordEncoderPort passwordEncoderPort) {
         this.userPersistencePort = userPersistencePort;
+        this.passwordEncoderPort = passwordEncoderPort;
     }
 
     @Override
     public void create(UserModel userModel) {
         UserModel user = userPersistencePort.getUserByEmail(userModel.getEmail());
         if (user != null) {
-            throw new SellerUserAlreadyExistsException();
+            throw new UserAlreadyExistsException();
         }
+
+        String encodedPassword = passwordEncoderPort.encode(userModel.getPassword());
+        userModel.setPassword(encodedPassword);
+
         userPersistencePort.create(userModel);
     }
-
 
 }
