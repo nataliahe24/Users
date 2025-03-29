0e1939857039fac8bd7d2f88c7f054fae52ef8ec
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 10:05:58 -0500

    refactor: rename UserModel

--- src/main/java/com/example/demo/users/application/mappers/UserDtoMapper.java
+++ src/main/java/com/example/demo/users/application/mappers/UserDtoMapper.java
@@ -2,12 +2,13 @@ package com.example.demo.users.application.mappers;
 
 import com.example.demo.users.application.dto.request.CreateUserRequest;
 import com.example.demo.users.application.dto.response.UserResponse;
+import com.example.demo.users.domain.model.UserModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserDtoMapper {
-    com.example.demo.users.domain.model.UserModel requestToModel(CreateUserRequest createdSellerUserRequest);
-    UserResponse modelToResponse(com.example.demo.users.domain.model.UserModel usersModel);
+    UserModel requestToModel(CreateUserRequest createdSellerUserRequest);
+    UserResponse modelToResponse(UserModel userModel);
 }
