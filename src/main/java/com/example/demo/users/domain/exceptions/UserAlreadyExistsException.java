c2a78fd0d707e360668b7abae895de786a1e08f6
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 10:03:22 -0500

    refactor: rename PhoneNumberCharacterInvalid exception

---
+++ src/main/java/com/example/demo/users/domain/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.demo.users.domain.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException() {
+        super();
+    }
+}
--- src/main/java/com/example/demo/users/infrastructure/exceptionshandler/ControllerAdvisor.java
+++ src/main/java/com/example/demo/users/infrastructure/exceptionshandler/ControllerAdvisor.java
@@ -11,13 +11,19 @@ import java.time.LocalDateTime;
 public class ControllerAdvisor {
 
     @ExceptionHandler(PhoneNumberExceededException.class)
-    public ResponseEntity<ExceptionResponse> handleDescriptionMaxSizeExceededException(PhoneNumberExceededException exception) {
+    public ResponseEntity<ExceptionResponse> handlePhoneMaxSizeExceededException(PhoneNumberExceededException exception) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(ExceptionConstants.PHONE_NUMBER_MAX_SIZE_MESSAGE,
                 LocalDateTime.now()));
     }
 
-    @ExceptionHandler(SellerUserAlreadyExistsException.class)
-    public ResponseEntity<ExceptionResponse> handleUsersAlreadyExistsException(SellerUserAlreadyExistsException exception) {
+    @ExceptionHandler(PhoneNumberCharacterInvalid.class)
+    public ResponseEntity<ExceptionResponse> handlePhoneNumberCharacterException(PhoneNumberCharacterInvalid exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(ExceptionConstants.PHONE_CHARACTER_INVALID_MESSAGE,
+                LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleUsersAlreadyExistsException(UserAlreadyExistsException exception) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(ExceptionConstants.USER_EXISTS_EXCEPTION,
                 LocalDateTime.now()));
     }
