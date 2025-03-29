0b6a61e7b29263263fa2d4b5633a9ad834e6a871
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-29 10:05:19 -0500

    refactor: rename UserAlreadyExistsException

--- src/main/java/com/example/demo/users/domain/exceptions/SellerUserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.users.domain.exceptions;
-
-public class SellerUserAlreadyExistsException extends RuntimeException {
-    public SellerUserAlreadyExistsException() {
-        super();
-    }
-}
