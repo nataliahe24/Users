c78453d94c6868096fd4444208c8a472c20ece27
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-01 18:10:27 -0500

    refactor:remove exception

--- src/main/java/com/example/demo/category/domain/exceptions/DescriptionMaxSizeExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.category.domain.exceptions;
-
-public class DescriptionMaxSizeExceededException extends RuntimeException {
-    public DescriptionMaxSizeExceededException() {
-        super();
-    }
-}
