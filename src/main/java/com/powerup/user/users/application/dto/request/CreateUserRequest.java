5fddc110644a0c8e8cbdfc125714a43a220bd660
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-17 17:26:39 -0500

    refactor: add Long role in CreateUserRequest

--- src/main/java/com/powerup/user/users/application/dto/request/CreateUserRequest.java
+++ src/main/java/com/powerup/user/users/application/dto/request/CreateUserRequest.java
@@ -5,5 +5,5 @@ package com.powerup.user.users.application.dto.request;
 import java.time.LocalDate;
 
 public record CreateUserRequest(String firstName, String lastName, Integer identityDocument, String phoneNumber,
-                                LocalDate birthDate, String email, String password, String role) {
+                                LocalDate birthDate, String email, String password, Long role) {
 }
