ea2de4112dfd4f01ecb08938042d42e658f2e97d
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-29 23:09:33 -0500

    feat: add security endpoint

--- src/main/java/com/powerup/user/users/infrastructure/endpoints/rest/UserController.java
+++ src/main/java/com/powerup/user/users/infrastructure/endpoints/rest/UserController.java
@@ -3,6 +3,8 @@ package com.powerup.user.users.infrastructure.endpoints.rest;
 import com.powerup.user.users.application.dto.request.CreateUserRequest;
 import com.powerup.user.users.application.dto.response.CreateUserResponse;
 import com.powerup.user.users.application.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,10 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/")
+    @Operation(
+            summary = "Endpoint protegido",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     public ResponseEntity<CreateUserResponse> create(@RequestBody CreateUserRequest createUserRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(createUserRequest));
     }
