67d41d5309257333afda4bc83847e284aecf8d1b
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-27 21:41:18 -0500

    feat: add  id and  name to LoginResponse

--- src/main/java/com/powerup/user/users/application/dto/response/LoginResponse.java
+++ src/main/java/com/powerup/user/users/application/dto/response/LoginResponse.java
@@ -1,4 +1,4 @@
 package com.powerup.user.users.application.dto.response;
 
-public record LoginResponse(String email, String message, String accessToken, Long role) {
+public record LoginResponse(Long id,String name, String email, String message, String accessToken, Long role) {
 }
--- src/main/java/com/powerup/user/users/application/services/impl/AuthServiceImpl.java
+++ src/main/java/com/powerup/user/users/application/services/impl/AuthServiceImpl.java
@@ -28,13 +28,13 @@ public class AuthServiceImpl implements AuthService {
         String email = request.email();
         String password = request.password();
 
-        // Obtener el usuario para autenticación y para acceder a su rol
+
         UserModel user = authServicePort.login(email, password);
         if (user == null) {
             throw new RuntimeException("Usuario no encontrado o contraseña incorrecta");
         }
 
-        // Crear la autenticación
+
         List<SimpleGrantedAuthority> authorities = Collections.singletonList(
             new SimpleGrantedAuthority(user.getRole().getName()));
         Authentication authentication = new UsernamePasswordAuthenticationToken(
@@ -43,14 +43,14 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String accessToken = jwUtils.createToken(authentication);
         
-        return new LoginResponse(email, "Usuario Logueado", accessToken, user.getRole().getId());
+        return new LoginResponse(user.getId(), user.getFirstName(), email, "Usuario Logueado", accessToken, user.getRole().getId());
     }
 
     public Authentication authenticate(String email, String password) {
-        System.out.println("Autenticando usuario con email: " + email);// prueba
+
         UserModel user = authServicePort.login(email, password);
         if (user == null) {
-            throw new RuntimeException("Usuario no encontrado o contraseña incorrecta"); // prueba
+            throw new RuntimeException("Usuario no encontrado o contraseña incorrecta");
         }
         List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getName()));
         return new UsernamePasswordAuthenticationToken(email, password, authorities);
